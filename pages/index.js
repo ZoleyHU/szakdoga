939a5e12dc20944fdca68c59e4bde0af79c597f0
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2023-03-24 11:07:17 +0100

    Legtöbbet értékelt szolgáltatás megjelenítésének javítása

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import factory from "../ethereum/factory";
-import {Button, Card, CardGroup, Container, Divider, Icon} from "semantic-ui-react";
+import {Button, Card, CardGroup, Container} from "semantic-ui-react";
 import Layout from "../components/Layout";
 import {Link} from "../routes";
 import Service from "../ethereum/service";
@@ -8,6 +8,8 @@ import Service from "../ethereum/service";
 class HomePage extends Component {
     static async getInitialProps() {
         const serviceAddresses = await factory.methods.getDeployedServices().call();
+        const mostRatedServiceAddress = await factory.methods.getMostRatedService().call();
+        let mostRatedService;
 
         const services = await Promise.all(
             Array(parseInt(serviceAddresses.length)).fill().map(async (element, index) => {
@@ -15,6 +17,18 @@ class HomePage extends Component {
                 const name = await currentServie.methods.name().call();
                 const tagged = await currentServie.methods.tagged().call();
                 const avgRating = await currentServie.methods.avgRating().call();
+
+                if (serviceAddresses[index] === mostRatedServiceAddress) {
+                    const reviews = await currentServie.methods.getReviewCount().call();
+
+                    mostRatedService = {
+                        serviceAddress: serviceAddresses[index],
+                        serviceName: name,
+                        tagged: tagged,
+                        avgRating: avgRating,
+                        reviewCount: reviews
+                    }
+                }
 
                 return {
                     serviceAddress: serviceAddresses[index],
@@ -24,56 +38,45 @@ class HomePage extends Component {
                 }
             })
         );
-        const mostRatedServiceAddress = await factory.methods.getMostRatedService().call();
-        const mostRatedService = Service(mostRatedServiceAddress);
-        const mostRatedServiceTagged = await mostRatedService.methods.tagged().call();
-        const mostRatedServiceName = await mostRatedService.methods.name().call();
-        const mostRatedServiceAvgRating = await mostRatedService.methods.avgRating().call();
-        const mostRatedServiceDetails = {
-            serviceAddress: mostRatedServiceAddress,
-            serviceName: mostRatedServiceName,
-            tagged: mostRatedServiceTagged,
-            avgRating: mostRatedServiceAvgRating
-        }
-        return {services, mostRatedServiceDetails};
+
+        return {services, mostRatedService};
     }
 
     renderItems() {
         const items = this.props.services.map((service, index) => {
-            const color = service.tagged ? 'red' : 'green';
-            return {
-                header: service.serviceName,
-                meta: service.avgRating + '/ 10',
-                extra: (
-                    <Link route={`/services/${service.serviceAddress}`}>
-                        <Button basic color={color}>Megtekintés</Button>
-                    </Link>
-                ),
-                color: color,
-                fluid: true
-            };
+            return this.renderSingleItem(service);
         });
 
         return <Card.Group items={items} itemsPerRow={2}/>
+    }
+
+    renderSingleItem(service) {
+        const color = service.tagged ? 'red' : 'green';
+        return {
+            header: service.serviceName,
+            meta: service.avgRating + '/ 10',
+            extra: (
+                <Link route={`/services/${service.serviceAddress}`}>
+                    <Button basic color={color}>Megtekintés</Button>
+                </Link>
+            ),
+            color: color,
+            fluid: true
+        };
+    }
+
+    renderMostRatedItem() {
+        let item = this.renderSingleItem(this.props.mostRatedService);
+        item.description = this.props.mostRatedService.reviewCount + " db értékeléssel a legértékeltebb szolgáltatás jelenleg az oldalon";
+        const items = [item]
+        return <CardGroup items={items}/>
     }
 
     render() {
         return (
             <Layout>
                 <Container textAlign='left' as='h3'>Legtöbbet értékelt szolgáltatás</Container>
-                <Card.Group itemsPerRow={1}>
-                    <Card
-                        fluid
-                        header={this.props.mostRatedServiceDetails.serviceName}
-                        meta={this.props.mostRatedServiceDetails.avgRating + '/ 10'}
-                        extra={
-                            <Link route={`/services/${this.props.mostRatedServiceDetails.serviceAddress}`}>
-                                <Button basic color={this.props.mostRatedServiceDetails.tagged ? 'red' : 'green'}>Megtekintés</Button>
-                            </Link>
-                        }
-                        color={this.props.mostRatedServiceDetails.tagged ? 'red' : 'green'}
-                    />
-                </Card.Group>
+                {this.renderMostRatedItem()}
                 <Container textAlign='left' as='h3'>Szolgáltatások</Container>
                 {this.renderItems()}
             </Layout>
