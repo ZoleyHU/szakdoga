39387bfc742a0251cdf27eadcf68d850499090a8
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2023-04-02 14:34:14 +0200

    log teszt

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const app = next({
     dev: process.env.NODE_ENV !== 'production'
 });
 
-console.log(app)
 const routes = require('./routes');
+console.log(routes)
 const handler = routes.getRequestHandler(app);
 
 const port = process.env.PORT || 3000;
+console.log(port)
 
 app.prepare().then(() => {
     express().use(handler).listen(port, (err) => {
