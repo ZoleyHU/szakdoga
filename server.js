3d0259fb28d75f3d25727068aff18d1731f10903
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2023-04-02 12:26:43 +0200

    server.js port javítás

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const {createServer} = require('http');
 const next = require('next');
 
 const app = next({
-    dev: process.env.NODE_ENV !== 'production'
+    dev: process.env.NODE_ENV !== 'production',
+    port: process.env.PORT
 });
 
 const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-    createServer(handler).listen(3000, (err) => {
+    createServer(handler).listen(app.port || 3000, (err) => {
         if (err) throw err;
         console.log('Ready on localhost:3000');
     });
