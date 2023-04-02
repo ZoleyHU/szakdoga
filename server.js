d032aa6fbbb866a594949bb7c22670436b432d1c
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2023-04-02 14:43:33 +0200

    production mode teszt

--- server.js
+++ server.js
@@ -3,18 +3,16 @@ const next = require('next');
 const express = require('express');
 
 const app = next({
-    dev: process.env.NODE_ENV !== 'production'
+    dev: process.env.NODE_ENV === 'production'
 });
 
 const routes = require('./routes');
-console.log(routes)
 const handler = routes.getRequestHandler(app);
 
 const port = process.env.PORT || 3000;
-console.log(port)
 
-app.prepare().then(() => {
-    express().use(handler).listen(port, (err) => {
+app.prepare().then(async () => {
+    await express().use(handler).listen(port, (err) => {
         if (err) throw err;
         console.log('Ready on port: '+port);
     });
