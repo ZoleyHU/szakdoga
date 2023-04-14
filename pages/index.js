0b7cc7030a55080cfa192ee7799cd9d119e5613d
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2023-04-14 07:47:32 +0200

    p-limit használata a getInitialPropsban lévő async await mennyiség miatt bekövetkező memory leak elkerülése érdekében

--- pages/index.js
+++ pages/index.js
@@ -4,41 +4,44 @@ import {Button, Card, CardGroup, Container} from "semantic-ui-react";
 import Layout from "../components/Layout";
 import {Link} from "../routes";
 import Service from "../ethereum/service";
+import pLimit from "p-limit";
 
 class HomePage extends Component {
     static async getInitialProps() {
+        const limit = pLimit(5);
         const serviceAddresses = await factory.methods.getDeployedServices().call();
         const mostRatedServiceAddress = await factory.methods.getMostRatedService().call();
         let mostRatedService;
 
         const services = await Promise.all(
             Array(parseInt(serviceAddresses.length)).fill().map(async (element, index) => {
-                const currentServie = Service(serviceAddresses[index]);
-                const name = await currentServie.methods.name().call();
-                const tagged = await currentServie.methods.tagged().call();
-                const avgRating = await currentServie.methods.avgRating().call();
+                return limit(async () => {
+                    const currentServie = Service(serviceAddresses[index]);
+                    const name = await currentServie.methods.name().call();
+                    const tagged = await currentServie.methods.tagged().call();
+                    const avgRating = await currentServie.methods.avgRating().call();
 
-                if (serviceAddresses[index] === mostRatedServiceAddress) {
-                    const reviews = await currentServie.methods.getReviewCount().call();
+                    if (serviceAddresses[index] === mostRatedServiceAddress) {
+                        const reviews = await currentServie.methods.getReviewCount().call();
 
-                    mostRatedService = {
+                        mostRatedService = {
+                            serviceAddress: serviceAddresses[index],
+                            serviceName: name,
+                            tagged: tagged,
+                            avgRating: avgRating,
+                            reviewCount: reviews
+                        }
+                    }
+
+                    return {
                         serviceAddress: serviceAddresses[index],
                         serviceName: name,
                         tagged: tagged,
-                        avgRating: avgRating,
-                        reviewCount: reviews
+                        avgRating: avgRating
                     }
-                }
-
-                return {
-                    serviceAddress: serviceAddresses[index],
-                    serviceName: name,
-                    tagged: tagged,
-                    avgRating: avgRating
-                }
+                })
             })
         );
-
         return {services, mostRatedService};
     }
 
