621f9809975014d2c7295c33f3d75642bfdc500c
Hegedűs Zoltán <h045746@stud.u-szeged.hu>
2023-05-03 09:31:46 +0200

    váltás sepoliára a factory.js-ben, valamint a szerződések címének jelölése kommentben

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,10 @@ import ServiceFactory from './build/ServiceFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(ServiceFactory.interface),
-    '0x55EbfbDd2FE5fE7B1aCFDB8fb17254F1Bf37ef08'
+    '0xAA8B052c5B431901219426571c53013Dd7b361a5'
 );
+
+//goerli: 0x55EbfbDd2FE5fE7B1aCFDB8fb17254F1Bf37ef08
+//sepolia: 0xAA8B052c5B431901219426571c53013Dd7b361a5
 
 export default instance;
